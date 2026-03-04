cb3da2e9bcb63b0daafdb4270098e518f68530eb
Lukas Behammer <lukas.behammer@uni-a.de>
2026-03-04 17:23:47 +0100

    docs: update sphinx config
    add logo, add github link to header, configure for readthedocs, change html title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -45,7 +47,24 @@ exclude_patterns = []
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_favicon = '../../branding/logo.png'
-html_title = f"{project} documentation v{release}"
+html_title = f"{project} v{release}"
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_logo = "../../branding/logo.png"
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/cimt-unia/pyTermite",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-square-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
+
 
 # -- Options for Autodoc -----------------------------------------------------
 autodoc_member_order = "groupwise"
