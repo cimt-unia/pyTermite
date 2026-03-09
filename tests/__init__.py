6f2ba8fee732f1e91d34c51dd58da1485cdce0fb
Lukas Behammer <lukas.behammer@uni-a.de>
2026-03-09 18:26:27 +0100

    chore: add __init__.py for tests

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2026 by Lukas Behammer
+#  University of Augsburg
+#  Department of Computer Science
+#  Chair of Informatics for Medical Technology
+#
+#  SPDX-License-Identifier: BSD-3-Clause
