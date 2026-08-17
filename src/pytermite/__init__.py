cdcaa33da8b6de4e508b71691e3eb7be0de4006d
Lukas Behammer <lukas.behammer@uni-a.de>
2026-08-17 15:06:51 +0200

    feat: add check if bluetooth is available on device

--- src/pytermite/__init__.py
+++ src/pytermite/__init__.py
@@ -1,4 +1,4 @@
-# ruff: noqa: RUF067
+# ruff: file-ignore[non-empty-init-module]
 """
 `pyTermite` is a small package to control multiple GoPro cameras via USB connection.
 
@@ -16,13 +16,43 @@ Examples
 #
 #  SPDX-License-Identifier: BSD-3-Clause
 
+import asyncio
+import os
 from importlib.metadata import PackageNotFoundError, version
+
+import structlog
+from bleak import BleakError, BleakScanner
+
+import pytermite.commands as commands
+import pytermite.config as config
+import pytermite.connection as connection
+import pytermite.utils as utils
+
+logger = structlog.get_logger(__name__)
 
 try:
     __version__ = version("pytermite")
 except PackageNotFoundError:
     # package is not installed
     pass
+
+
+async def _is_bluetooth_available() -> None:
+    try:
+        # Try to instantiate a BleakScanner (does not require a device to be powered on)
+        scanner = BleakScanner()
+        await scanner.start()
+        await scanner.stop()
+        await logger.adebug("Bluetooth is available.")
+        os.environ["BLUETOOTH_AVAILABLE"] = "true"
+    except BleakError as e:
+        await logger.adebug("Bluetooth is not available.", error=str(e))
+        os.environ["BLUETOOTH_AVAILABLE"] = "false"
+
+
+# Run the Bluetooth availability check asynchronously at module import time
+asyncio.run(_is_bluetooth_available())
+
 
 __author__ = "Lukas Behammer"
 
@@ -33,11 +63,6 @@ __all__ = [
     "get_version",
     "utils",
 ]
-
-import pytermite.commands as commands
-import pytermite.config as config
-import pytermite.connection as connection
-import pytermite.utils as utils
 
 
 def get_version() -> str:
